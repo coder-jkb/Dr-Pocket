f1cdae8c106e901af63237a54c62b066d6518781
PrachetShah <prachetshah25@gmail.com>
2021-08-14 21:47:13 +0530

    Added a features section

--- UI/src/components/App.js
+++ UI/src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Home from './home'
+import Features from './features'
 
 function App() {
   return (
     <div>
      <Home/>
+     <Features/>
     </div>
-  )
-}
+
+  );
+};
 
 export default App
